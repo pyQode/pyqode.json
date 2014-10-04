49c407bfad5c14735adb0f33397e4cd0483a5232
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-10-04 16:20:49 +0200

    Works better when uncommented ;)

--- examples/json_editor.py
+++ examples/json_editor.py
@@ -23,10 +23,10 @@ class Window(QtWidgets.QMainWindow):
         self.editor.file.open(
             os.path.abspath(os.path.join(
                 '..', 'test', 'files', 'example.json')))
-        # pygment_style = random.choice(modes.PYGMENTS_STYLES)
-        # logging.info('pygments style: %s', pygment_style)
-        # self.editor.syntax_highlighter.color_scheme = api.ColorScheme(
-        #     pygment_style)
+        pygment_style = random.choice(modes.PYGMENTS_STYLES)
+        logging.info('pygments style: %s', pygment_style)
+        self.editor.syntax_highlighter.color_scheme = api.ColorScheme(
+            pygment_style)
 
 
 logging.basicConfig(level=logging.INFO)
