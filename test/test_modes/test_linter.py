44fcfe8d8e26821c26e6d05efc2ef703edd72f33
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2015-09-18 09:09:09 +0200

    Fix tests

--- test/test_modes/test_linter.py
+++ test/test_modes/test_linter.py
@@ -5,16 +5,14 @@ from pyqode.json.modes.linter import json_linter
 def test_valid_json():
     with open('test/files/example.json', 'r') as f:
         code = f.read()
-    status, errors = json_linter({'code': code})
-    assert status is True
+    errors = json_linter({'code': code})
     assert len(errors) == 0
 
 
 def test_invalid_json():
     with open('test/files/invalid.json', 'r') as f:
         code = f.read()
-    status, errors = json_linter({'code': code})
-    assert status is True
+    errors = json_linter({'code': code})
     assert len(errors) == 1
     assert isinstance(errors[0][0], str)
     assert errors[0][1] == CheckerMessages.ERROR
